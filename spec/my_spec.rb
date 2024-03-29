55a3a7236023b6959cd35e730e25042d98a1fca9
benk-gc <bkyriakou@gocardless.com>
2024-03-29 22:15:22 +0000

    Is it Zeus?

--- spec/my_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-RSpec.describe "a control test without wrapping" do
-  it { expect(true).to be(false) }
-
-  after { RSpec.current_example.metadata[:extra_failure_lines] = "foo\nbar" }
-end
